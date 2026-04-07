5e1d24630aaccb30907da3a3c03077f53537bed6
reGrach <regrachdev@yandex.ru>
2026-04-07 19:58:46 +0300

    7.04

--- index.js
+++ index.js
@@ -1,16 +1,49 @@
+const taskListEl = document.getElementById("task-list");
+const addButton = document.getElementById("add-btn");
+const inputTaskEl = document.getElementById("input-task");
+addButton.onclick = addTask;
+
+let tasks = [
+    {
+        title: "Купить зимние ботинки",
+        priority: "low",
+        isCompleted: false,
+    },
+    {
+        title: "Убраться дома",
+        priority: "medium",
+        isCompleted: true,
+    },
+    {
+        title: "Поспать",
+        priority: "high",
+        isCompleted: false,
+    }
+];
+
+tasks.forEach(task => createTaskElement(task));
 
 function start() {
     console.log('Hello world');
     alert('111111111');
 }
 
-const taskListEl = document.getElementById("task-list");
-const addButton = document.getElementById("add-btn");
-const inputTaskEl = document.getElementById("input-task");
-addButton.onclick = addTask;
+document.addEventListener('keydown',
+    (ev) => {
+        if (ev.key === 'Enter') {
+            addTask();
+        }
+    })
 
 function addTask() {
     const taskTitle = inputTaskEl.value;
+    if (taskTitle) {
+
+        inputTaskEl.value = null;
+    }
+}
+
+function createTaskElement(task) {
     const newTaskEl = document.createElement('li');
     newTaskEl.classList.add("task-item");
     newTaskEl.classList.add("low-priority");
@@ -20,7 +53,7 @@ function addTask() {
     });
 
     newTaskEl.innerHTML = `
-    <span class="task-content">${taskTitle}</span>
+    <span class="task-content">${task.title}</span>
     <div class="task-actons">
         <button class="task-btn" onclick="this.parentNode.parentNode.remove()">
             <span class="material-symbols-outlined">delete</span>
@@ -28,6 +61,3 @@ function addTask() {
     </div>`
     taskListEl.append(newTaskEl);
 }
-
-
-
